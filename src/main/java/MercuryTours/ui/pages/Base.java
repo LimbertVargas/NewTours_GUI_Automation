9cd11d52cb3bff25a1147b5cbbc6b1ac5e0775de
LIMBERT <limbert.vargas@fundacion-jala.org>
2019-09-09 00:42:01 -0400

    add LoginPage class

--- src/main/java/MercuryTours/ui/pages/Base.java
+++ src/main/java/MercuryTours/ui/pages/Base.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
 public class Base {
-    private WebDriver driver;
+    public WebDriver driver;
 
     public Base(WebDriver driver) {
         this.driver = WebDriverManager.getInstance().getWebDriver();
--- src/main/java/MercuryTours/ui/pages/LoginPage.java
+++ src/main/java/MercuryTours/ui/pages/LoginPage.java
@@ -1,4 +1,43 @@
 package MercuryTours.ui.pages;
 
-public class LoginPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends Base {
+
+    @FindBy(name = "userName")
+    private WebElement userNameTxtBx;
+
+    @FindBy(name = "password")
+    private WebElement passwdTxtBx;
+
+    @FindBy(name = "login")
+    private WebElement signInBtn;
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void login(final String userName, final String password) {
+        setUserName(userName);
+        setPassword(password);
+        clickSignInBtn();
+    }
+
+    public void setUserName(final String userName) {
+        userNameTxtBx.sendKeys(userName);
+    }
+
+    public void setPassword(final String password) {
+        passwdTxtBx.sendKeys(password);
+    }
+
+    public void clickSignInBtn() {
+        signInBtn.click();
+    }
+
+    public void closeWindow() {
+        driver.close();
+    }
 }
