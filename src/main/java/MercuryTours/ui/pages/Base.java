d8cd41ab34a2f5b6b7f45fdb6b730aeba1c75c1a
LIMBERT <limbert.vargas@fundacion-jala.org>
2019-09-08 23:34:08 -0400

    adding base class

---
+++ src/main/java/MercuryTours/ui/pages/Base.java
@@ -0,0 +1,14 @@
+package MercuryTours.ui.pages;
+
+import core.selenium.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class Base {
+    private WebDriver driver;
+
+    public Base(WebDriver driver) {
+        this.driver = WebDriverManager.getInstance().getWebDriver();
+        PageFactory.initElements(driver,this);
+    }
+}
